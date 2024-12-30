ab9f4dc93d34ddfd7f86d6b5c50e2e80f58dcf03
mmgov <40439774+mmgov@users.noreply.github.com>
2024-12-30 17:57:02 +0000

    uploading first files

---
+++ app.R
@@ -0,0 +1,28 @@
+library("DT")
+library("tidyverse")
+library(teal.modules.general)
+
+#ADSL <- pharmaverseadam::adsl
+
+
+nero <- read_csv("./england_ks5final.csv", na = c('*','','na')) %>% 
+  select('REGION','ESTAB','SCHNAME','TALLPPE_ALEV_1618','TALLPUP_ALEV_1618','NFTYPE/FESITYPE','TOWN','PCON_NAME')%>%
+  rename(SCHOOLTYPE = 'NFTYPE/FESITYPE')%>% 
+  filter (
+    !TALLPPE_ALEV_1618 %in% c('NE','SUPP',NA)
+    ,ESTAB != 'NA'
+    ,!SCHOOLTYPE %in% c('SS','3066905','3104027','AC1619','Agriculture and Horticulture College'
+                        ,'Art, Design and Performing Arts College','CTC','MODFC',NA))%>%
+  mutate_at(c('TALLPPE_ALEV_1618', 'TALLPUP_ALEV_1618'), as.numeric)
+
+app <- teal::init(
+  data = cdisc_data(
+    ADSL = example_cdisc_data("nero")
+  ),
+  modules = teal::modules(
+    tm_data_table()
+    # ,
+    # tm_variable_browser()
+  )
+)
+shinyApp(app$ui, app$server)
---
+++ test.R
@@ -0,0 +1,26 @@
+library("DT")
+library("tidyverse")
+library(teal.modules.general)
+library(sparkline)
+
+alc<-read_csv("./england_ks5final.csv", na = c('*','','na')) %>% 
+  select('REGION','ESTAB','SCHNAME','TALLPPE_ALEV_1618','TALLPUP_ALEV_1618','NFTYPE/FESITYPE','TOWN','PCON_NAME')%>%
+  rename(SCHOOLTYPE = 'NFTYPE/FESITYPE')%>% 
+  filter (
+    !TALLPPE_ALEV_1618 %in% c('NE','SUPP',NA)
+    ,ESTAB != 'NA'
+    ,!SCHOOLTYPE %in% c('SS','3066905','3104027','AC1619','Agriculture and Horticulture College'
+                        ,'Art, Design and Performing Arts College','CTC','MODFC',NA))%>%
+  mutate_at(c('TALLPPE_ALEV_1618', 'TALLPUP_ALEV_1618'), as.numeric)
+
+app <- teal::init(
+  data = teal_data(
+    iris = iris,
+    alc = alc
+  ),
+  modules = teal::modules(
+    tm_data_table(),
+    tm_variable_browser()
+  )
+)
+shinyApp(app$ui, app$server)
